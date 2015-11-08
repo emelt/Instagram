1d618df001a75361d7be3a8141024eb051410536
Emel Topaloglu <emeltopaloglu@gmail.com>
2015-11-08 21:14:35 +0200

    Added search request for hashtags

--- Instagram/Instagram/Networking/Managers/INSPhotosManager.h
+++ Instagram/Instagram/Networking/Managers/INSPhotosManager.h
@@ -9,11 +9,24 @@
 @class INSPhoto;
 
 typedef void(^INSGetPhotosRequestCompletion)(NSError *error, NSArray<INSPhoto *> *photos);
+typedef void(^INSGetPhotosRequestCompletionWithPagination)(NSError *error, NSArray<INSPhoto *> *photos, NSString *nextMaxTagId);
 
 @interface INSPhotosManager : INSNetworkManager
 
+/**
+ Shared instance of photos manager, use for request cancellation
+ */
 + (instancetype)sharedInstance;
 
+/**
+ Fetches array of popular public photos
+ */
 - (NSString *)getPopularPhotosWithCompletion:(INSGetPhotosRequestCompletion)completion;
+
+/**
+ Searches photos with given keyword, returns results ordered from newest to oldest
+ Use maxTagId of previous response to fetch next group of results
+ */
+- (NSString *)getPhotosWithKeyword:(NSString *)keyword maxTagId:(NSString *)maxTagId completion:(INSGetPhotosRequestCompletionWithPagination)completion;
 
 @end
--- Instagram/Instagram/Networking/Managers/INSPhotosManager.m
+++ Instagram/Instagram/Networking/Managers/INSPhotosManager.m
@@ -6,14 +6,21 @@
 //  Copyright © 2015 Emel Topaloglu. All rights reserved.
 //
 
+#import "NSString+INSAdditions.h"
 #import "INSNetworkManager.h"
 #import "INSPhotosManager.h"
 #import "INSRequest.h"
 #import "INSPhoto.h"
 
 //https://api.instagram.com/v1/media/popular?client_id=358150b5ca1a41c38b0a99a47e6557a4
+//https://api.instagram.com/v1/tags/nofilter/media/recent?client_id=358150b5ca1a41c38b0a99a47e6557a4&&max_tag_id=1113882942266843447
 
 static NSString * const kINSPopularItemsURL = @"media/popular";
+static NSString * const kINSRecentMediaURL = @"media/recent";
+static NSString * const kINSTagsURL = @"tags";
+static NSString * const kINSNextMaxTagIDKey = @"max_tag_id";
+static NSString * const kINSPaginationKey = @"pagination";
+static NSString * const kINSCountKey = @"count";
 
 @implementation INSPhotosManager
 
@@ -34,7 +41,7 @@ static NSString * const kINSPopularItemsURL = @"media/popular";
 {
     INSRequest *popularPhotosRequest = [[INSRequest alloc] initWithRequestType:INSRequestTypeGet relativeURL:kINSPopularItemsURL];
     
-    popularPhotosRequest.additionalParameters[@"max_id"] = @"13872296";
+    popularPhotosRequest.additionalParameters[kINSCountKey] = @"100";
     
    return [self sendRequest:popularPhotosRequest completion:^(NSError *error, id data)
      {
@@ -58,7 +65,48 @@ static NSString * const kINSPopularItemsURL = @"media/popular";
              }
          }
      }];
+}
+
+
+- (NSString *)getPhotosWithKeyword:(NSString *)keyword maxTagId:(NSString *)maxTagId completion:(INSGetPhotosRequestCompletionWithPagination)completion
+{
+    if (!keyword)
+    {
+        completion ([NSError errorWithDomain:kINSNetworkingErrorDomain code:INSNetworkErrorInvalid userInfo:nil], nil, nil);
+    }
     
+    NSString *relativeURL = [NSString urlPathWithComponents:@[kINSTagsURL, keyword, kINSRecentMediaURL]];
+    INSRequest *photosRequest = [[INSRequest alloc] initWithRequestType:INSRequestTypeGet relativeURL:relativeURL];
+    
+    if (maxTagId)
+    {
+        
+        photosRequest.additionalParameters[kINSNextMaxTagIDKey] = maxTagId;
+    }
+    
+    return [self sendRequest:photosRequest completion:^(NSError *error, id data)
+            {
+                if (completion)
+                {
+                    if (error)
+                    {
+                        completion(error, nil, nil);
+                    }
+                    else
+                    {
+                        NSArray *photosArray = [data arrayForKey:kINSDataKey];
+                        NSMutableArray *mutablePhotos = [NSMutableArray array];
+                        
+                        [photosArray enumerateObjectsUsingBlock:^(NSDictionary *  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+                            INSPhoto *photo = [[INSPhoto alloc] initWithDictionary:obj];
+                            [mutablePhotos addObject:photo];
+                        }];
+                        
+                        NSString *nextMaxTagId = [[data dictionaryForKey:kINSPaginationKey] stringForKey:kINSNextMaxTagIDKey];
+                        completion(nil, [mutablePhotos copy], nextMaxTagId);
+                    }
+                }
+            }];
 }
 
 @end
